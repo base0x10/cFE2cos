b5203e19d8385fc8af97b01236de8a103895f07b
Zach Day <zachdayz@gmail.com>
2017-06-16 12:14:14 -0400

    Increase legibility of build scripts and output
    `cFE2cos.sh` uses an `if`-`elif` chain to check its arguments. @Others and I
    have concluded that a more conventional `case` block adheres to common
    shell scripting standards, so that change has been made.
    
    I removed a log message in `make.py` that reported the status of
    filesystem integration; such error checking is already performed by
    `check_call`.
    
    The `cFE2cos` method now checks the `UNIT_TEST` environment variable rather
    than the `-u` flag to imply that it should only build unit tests as a
    part of the unit test build process (that is, it should not be invoked
    manually).

--- build/make.py
+++ build/make.py
@@ -153,7 +153,6 @@ else:
     sp.check_call("make" + OUT, shell=True, cwd=COMPOSITE_MAKE_ROOT)
 sp.check_call("make cp" + OUT, shell=True, cwd=COMPOSITE_MAKE_ROOT)
 
-print "integrating cFE file system"
 sp.check_call("tar cf cFEfs.tar cf/" + OUT, shell=True, cwd=CFE_OBJECT_LOCATION)
 sp.check_call("mv cFEfs.tar ../../../../cFEfs.tar" + OUT, shell=True, cwd=CFE_OBJECT_LOCATION)
 sp.check_call("ld -r -b binary cFEfs.tar -o cFEfs.o" + OUT, shell=True, cwd=ROOT)
@@ -162,4 +161,3 @@ sp.check_call("cp mergedbooter.o composite/transfer/cFE_booter.o" + OUT, shell=T
 os.remove(ROOT+"cFEfs.o")
 os.remove(ROOT+"cFEfs.tar")
 os.remove(ROOT+"mergedbooter.o")
-
